21914875739497246e7bf6c7b19827dc6ff31547
jiwon <jidole041214@naver.com>
2021-03-24 19:51:19 +0900

    [MODIFY] : APP.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import './App.css';
 
 declare global {
@@ -8,48 +7,8 @@ declare global {
 }
 
 function App() {
-  useEffect(() => {
-    let container = document.getElementById('map');
-    let options = {
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3
-    };
-
-    let map = new window.kakao.maps.Map(container, options);
-
-    let geocoder = new window.kakao.maps.services.Geocoder();
-
-    // 주소로 좌표를 검색합니다
-    geocoder.addressSearch('제주특별자치도 제주시 첨단로 242', function(result:any, status:any) {
-
-      // 정상적으로 검색이 완료됐으면 
-      if (status === window.kakao.maps.services.Status.OK) {
-
-          var coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
-
-          // 결과값으로 받은 위치를 마커로 표시합니다
-          var marker = new window.kakao.maps.Marker({
-              map: map,
-              position: coords
-          });
-
-          // 인포윈도우로 장소에 대한 설명을 표시합니다
-          var infowindow = new window.kakao.maps.InfoWindow({
-              content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
-          });
-          infowindow.open(map, marker);
-
-          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-          map.setCenter(coords);
-      } 
-  });
-
-  }, [])
   return (
     <>
-    <div className="App">
-      <div id="map" style={{ width: "100vw", height: "100vh" }} />
-    </div>
     </>
   );
 }
